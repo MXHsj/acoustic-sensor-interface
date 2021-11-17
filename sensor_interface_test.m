2314506e417563bed0acb986b81104fad64cf33f
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2021-11-17 16:06:18 -0500

    Update sensor_interface_test.m

--- sensor_interface_test.m
+++ sensor_interface_test.m
@@ -2,14 +2,21 @@
 clc; clear; close all
 
 port = 'COM3';
-s = serialport(port,9600,'DataBits',8,'Parity','none','StopBits',1);
+sensors = serialport(port,9600,'DataBits',8,'Parity','none','StopBits',1);
 
-data = read(s,10,"double");
-data
+if ~isempty(sensors)
+    fclose(sensors);
+    delete(sensors);
+end
 
-% fopen(s);
-% msg = fscanf(s);
-% disp(msg)
-% fclose(s);
-% delete(s);
-% clear s;
+fopen(sensors); % initiate grbl1 communication
+pause(5);
+while sensors.BytesAvailable > 0 %& grbl2.BytesAvailable > 0
+    echo = fscanf(sensors);
+    disp(echo);
+end
+
+fclose(sensors);
+delete(ssensors);
+
+% data = read(sensors,10,"double");
