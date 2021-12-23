18eda7581e81acdb73daf659abbd883fc9fda98e
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2021-12-22 21:45:10 -0500

    add real-time visualization

--- ch101_franka_bridge.m
+++ ch101_franka_bridge.m
@@ -1,6 +1,17 @@
 %% read ch101 sensor values from serial input
 clc; clear; close all
 
+%% ----------------- ROS network -----------------
+rosshutdown
+% [~, local_ip] = system('ipconfig');
+setenv('ROS_MASTER_URI','http://130.215.121.229:11311') % ip of robot desktop
+setenv('ROS_IP','130.215.192.178')   % ip of this machine
+rosinit
+% define publisher
+ch101_pub = rospublisher('ch101', 'std_msgs/Float64MultiArray');
+ch101_msg = rosmessage(ch101_pub);
+ch101_msg.Data = [nan, nan, nan, nan];
+
 %% 
 port = 'COM3';
 n_sensors = 4;
@@ -30,6 +41,8 @@ while 1
     fprintf('port1 range: [mm]: %f \n', dist(2))
     fprintf('port2 range: [mm]: %f \n', dist(3))
     fprintf('port3 range: [mm]: %f \n', dist(4))
+    
+    ch101_msg.data = dist;
+    send(ch101_pub, ch101_msg);
     toc;
 end
-
